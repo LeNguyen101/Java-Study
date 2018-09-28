a2c8036ecaaf5b64ceddca38f649014750e6e4c1
LeNguyen101 <34471034+LeNguyen101@users.noreply.github.com>
2018-09-28 15:26:43 +0900

    Add files via upload

---
+++ Loops/WhileWhileLoop.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class DoWhileLoop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class WhileWhileLoop {
+    public static void main(String[] args) {
+        double[] temperatures = {25.0,19.0,4.0,1.0};
+        for(int i = 0; i < temperatures.length; i++){
+            System.out.println("temperatures is: "+ temperatures[i]);
+            
+        }
+    }
+}
+    
---
+++ Loops/ex2.java
@@ -0,0 +1,7 @@
+public class ex2 {
+    public static void main(String[] args) {
+        System.out.println("xxxx");
+        System.out.println(" ");
+        System.out.println("xxxx");
+       }
+}
